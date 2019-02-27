c5dd515b1c274ed0a0181a71ef77328718d76ea9
Oleksandr Chalyi <ochal@softserveinc.com>
2019-02-27 09:52:35 +0200

    webpack.config add

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const webpack = require('webpack');
+const path = require('path');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new LodashModuleReplacementPlugin
+  ]
+}
