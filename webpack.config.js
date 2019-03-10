87546db8c72201145656f2c62c2bca77b5f06417
root <root@DESKTOP-TARTQT6.localdomain>
2019-03-10 13:42:08 +0200

    some

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const webpack = require('webpack');
+const path = require('path');
+const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+
+  entry: './app/app.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+  watch:true,
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader" 
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      }
+    ],
+  },
+  plugins: [
+    new LodashModuleReplacementPlugin,
+    new MiniCssExtractPlugin({
+      filename: "dist/bundle.css",
+      chunkFilename: "bundle.css"
+    })
+  ]
+}
+
